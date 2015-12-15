3748fd1e2442a167ee8a1d4c7dc7531a66566367
Wertros <adam.masnik@gmail.com>
2015-12-15 19:30:18 +0100

    Stworzenie Spring boota

---
+++ src/main/java/hello/GreetingControler.java
@@ -0,0 +1,26 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+/**
+ * Created by Adam on 15.12.2015.
+ */
+
+@Controller
+public class GreetingControler {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @RequestMapping("/")
+    public String greet() {
+        return "redirect:greeting";
+    }
+}
